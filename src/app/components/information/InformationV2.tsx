b22222e1b54307228a209a9bd053f1e3a636cc5b
Brian Rico <114421216+Bricret@users.noreply.github.com>
2024-04-19 21:17:53 -0600

    Modificar tamaño de imagen principal en la sección de información del producto

--- src/app/components/information/InformationV2.tsx
+++ src/app/components/information/InformationV2.tsx
@@ -40,7 +40,7 @@ export default function InformationV2() {
                     alt="Lenovo XT80"
                     width={500}
                     height={400}
-                    className="rounded-lg object-cover w-full h-full md:h-60"
+                    className="rounded-lg object-cover w-full h-full md:h-80"
                 />
             </motion.div>
         </div>
